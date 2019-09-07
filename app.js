31bb395039eafff492be6ca97974e6af3736c5ec
phish <phishchiang@gmail.com>
2019-09-07 23:28:36 +0800

    try the wws by setting secure to ture

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const OSC = require('osc-js');
-const config = { udpClient: { port: 9129 } };
+const config = { udpClient: { port: 9129, secure: true } };
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) });
 
 osc.open(); // start a WebSocket server on port 8080
