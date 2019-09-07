00b386e3ce7859dddcd4b54d8772f7a20be32f1c
phish <phishchiang@gmail.com>
2019-09-07 22:33:16 +0800

    ready to deploy

--- app.js
+++ app.js
@@ -3,33 +3,33 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-const OSC = require('osc-js')
-const config = { udpClient: { port: 9129 } }
-const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
+const OSC = require('osc-js');
+const config = { udpClient: { port: 9129 } };
+const osc = new OSC({ plugin: new OSC.BridgePlugin(config) });
 
-osc.open() // start a WebSocket server on port 8080
+osc.open(); // start a WebSocket server on port 8080
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, './public/index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log(socket.id);
-  socket.on('mouse',function(obj){
+  socket.on('mouse', function(obj) {
     console.log(obj);
-    io.emit('FINAL',obj);
+    io.emit('FINAL', obj);
   });
 });
 
