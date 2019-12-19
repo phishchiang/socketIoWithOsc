78b04de0d82529f854333aec299ae91a6bfde407
phish <phishchiang@gmail.com>
2019-12-19 13:51:08 +0800

    Added the markdown file

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
 
-const OSC = require('osc-js');
+const OSC = require("osc-js");
 const config = { udpClient: { port: 9129, secure: true } };
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) });
 
 osc.open(); // start a WebSocket server on port 8080
 
-osc.on('/test/random', message => {
+osc.on("/test/random", message => {
   console.log(message.args); // prints the message arguments
+  console.log("好想睡覺喔");
 });
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, './public/index.html');
+const INDEX = path.join(__dirname, "./public/index.html");
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX))
@@ -23,22 +24,22 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', socket => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on("connection", socket => {
+  console.log("Client connected");
+  socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
 
-io.on('connection', function(socket) {
+io.on("connection", function(socket) {
   console.log(socket.id);
-  socket.on('mouse', function(obj) {
+  socket.on("mouse", function(obj) {
     console.log(obj);
-    io.emit('FINAL', obj);
+    io.emit("FINAL", obj);
   });
-  socket.on('gogogo', function(obj) {
+  socket.on("gogogo", function(obj) {
     console.log(obj);
-    io.emit('FINAL', obj);
+    io.emit("FINAL", obj);
   });
 });
 
